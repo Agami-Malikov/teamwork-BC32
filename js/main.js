fe2bc7cacc4c865c3795df4cc3abdc02a500c1ab
Agami-Malikov <agami.malikov@gmail.com>
2022-04-09 23:27:47 +0300

    слайдер на hero

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+$('.hero__slider-list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 1700,
+  infinite: false,
+});
+
+});
+ 
